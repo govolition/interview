8a2b3c7591a4a86062a0050e9291233e3ce42fd4
Luke Dennis <luke@govolition.com>
2023-05-19 13:18:08 -0600

    feat: add boilerplate for fetch and Express JSON

--- index.ts
+++ index.ts
@@ -1,24 +1,30 @@
 import express from 'express';
 import path from 'path';
 
-import { insertDummyData, setup, testGetAllRows } from './lib/db';
+import { exampleGetAllRows, exampleInsertRow, setup } from './lib/db';
 
 const app = express();
 const port = 3000;
 
-
 // Serve the contents of the /public directory
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 app.get('/db', async (req, res) => {
-  const rows = await testGetAllRows()
-  res.setHeader('content-type', 'application/json')
-  res.send(rows);
+  const rows = await exampleGetAllRows()
+  res.json(rows);
+});
+
+app.post('/insert', async (req, res) => {
+  const payloadObject = req.body;
+  res.json({message: "received", payloadObject});
 });
 
 async function main() {
+  // Note: in-memory database is reset when process is restarted
   await setup()
-  await insertDummyData()
+  await exampleInsertRow('hi', 'hello world');
+
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}.`);
   });
---
+++ public/fetch.js
@@ -0,0 +1,13 @@
+async function getJson(url) {
+  const response = await fetch(url);
+  return response.json();
+}
+
+async function postJson(url, payload) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(payload)
+  });
+  return response.json();
+}
